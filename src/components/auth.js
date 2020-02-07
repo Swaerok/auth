90d3269d40ca1269c5b1be83fde4521589df5f0e
Swaerok <swaer19950501@gmail.com>
2020-02-07 16:35:06 +0300

    Some fix

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,22 +1,45 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React, { useState } from 'react';
-import psevdoServer from './server'
+import {history} from './history'
+import { psevdoServer, registration } from './server'
+import Enter from './enter'
+import Register from './register'
+import styles from './auth.scss'
 
 export default function Auth() {
-	const [login, setLogin] = useState('');
+	const [login, setEmail] = useState('');
 	const [pass, setPass] = useState('');
+	const [reg, useReg] = useState(false)
+	const [successReg, useSuccessReg] = useState(false)
 
 	const res = (e) => {
 		e.preventDefault();
-		
-		psevdoServer(login, pass).then((result) => console.log(result)).catch(console.log())
+		psevdoServer(login, pass).then((result) => history.push('/anime')).catch(console.log())
 	}
-
+	const regist = () => {
+		useReg(true)
+	}
+	const enter = () => {
+		useReg(false)
+	}
 	return (
-		<form onSubmit={res}>
-			<input onChange={(e) => { setLogin(e.target.value) }} value={login} />
-			<input onChange={(e) => { setPass(e.target.value) }} value={pass} />
-			<button type="submit">Add</button>
-		</form>
+		<div className={styles.auth}>
+			{successReg ? <h3>Вы успешно зарегистрировались</h3> : ''}
+			<div className={styles.auth__buttons}>
+				<button onClick={enter}>Вход</button>
+				<button onClick={regist}>Регистрация</button>
+			</div>
+			{!reg
+				? <Enter
+					useSuccessReg={useSuccessReg}
+					res={res}
+					login={login}
+					setEmail={setEmail}
+					setPass={setPass}
+					pass={pass} />
+				:	<Register useSuccessReg={useSuccessReg} enter={enter} register={registration} />
+			}
+
+		</div>
 	)
 }
