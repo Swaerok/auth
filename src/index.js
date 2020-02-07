0aa7e53873fadec2c5179905733184173a96baf5
Swaerok <swaer19950501@gmail.com>
2020-02-07 16:31:46 +0300

    Some fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+
 import './style.scss'
 import { App } from './components/app'
+
 
 ReactDOM.render(
 	<App />,
