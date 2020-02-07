d46778ab68627956ea258809448ad9ac63081650
Swaerok <swaer19950501@gmail.com>
2020-02-07 16:32:23 +0300

    Add router in App

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,24 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
+import {
+	Router,
+	Switch,
+	Route,
+} from 'react-router-dom';
+import {history} from './history'
 import styles from './app.scss'
 import Auth from './auth'
+import Anime from './anime'
 
 export const App = () => {
 	return (
-		<div>
-			<Auth />
-		</div>
+		<Router history={history}>
+			<div className={styles.app}>
+				<Switch>
+					<Route exact path="/" component={Auth} />
+					<Route path="/anime" component={Anime} />
+				</Switch>
+			</div>
+		</Router>
 	)
 }
